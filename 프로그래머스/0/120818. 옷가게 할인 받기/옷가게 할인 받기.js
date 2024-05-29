59ec985543c77a4c7ab364eca094489c66ededed
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-05-29 11:59:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,10 @@
+function solution(price) {
+    if (price >= 100000 && price < 300000) {
+        return ~~(price - price * 0.05);
+    } else if (price >= 300000 && price < 500000) {
+        return ~~(price - price * 0.1);
+    } else if (price >= 500000) {
+        return ~~(price - price * 0.2);
+    }
+    return price;
+}
