d8d72f516af36f639e79d131af136b35c5d6473c
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-06-03 23:29:34 +0900

    [level 0] Title: 옹알이 (1), Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -2,6 +2,7 @@ function solution(babbling) {
     const arr = ["aya", "ye", "woo", "ma"];
     return babbling.filter(v => {
         for(i=0; i<arr.length; i++) {
+            // 공백 넣어주기,,
             v = v.split(arr[i]).join(' ');
         }
         return v.trim() === '';
