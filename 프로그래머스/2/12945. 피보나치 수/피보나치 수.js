863f3e6cfda403bb39be1dbe46360c6a44fbb751
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-06-28 09:15:56 +0900

    [level 2] Title: 피보나치 수, Time: 4.61 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let result = [0, 1];
+    for(i=2; i<=n; i++) {
+        let sum = result[i-2] + result[i-1];
+        result.push(sum % 1234567)
+    }
+    return result[n];
+}
