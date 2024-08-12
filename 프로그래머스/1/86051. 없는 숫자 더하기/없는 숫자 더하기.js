0b49a55348b252dec69323e10f242900c10238b4
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-08-12 19:28:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,10 +1,9 @@
 function solution(numbers) {
-    const arr = [0,1,2,3,4,5,6,7,8,9]
     let result= [];
     
     for(i=0; i<10; i++) {
-        if(!numbers.includes(arr[i])) {
-            result.push(arr[i])
+        if(!numbers.includes(i)) {
+            result.push(i)
         }
     }
     return result.reduce((a,b) => a+b);
