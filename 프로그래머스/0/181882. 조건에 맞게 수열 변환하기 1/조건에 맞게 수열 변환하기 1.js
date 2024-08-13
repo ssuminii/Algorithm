0f20b09ffe6efa584d0c5ffff49b449563f0bd87
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-08-13 14:21:28 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 70.17 ms, Memory: 45.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    return arr.map(v => {
+        if(v >= 50 && v%2===0) {
+            return v/2
+        } else if(v < 50 && v%2 !== 0) {
+            return v*2
+        } else {
+            return v
+        }
+    })
+}
