86f7aa2e49647d3d61da6077f9f106b465ea624c
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-08-13 15:05:18 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,4 @@
 function solution(phone_number) {
-    
     let a = [...phone_number].slice(0,phone_number.length-4).join('').replace(/[0-9]/g,'*')
     let b = [...phone_number].slice(phone_number.length-4,phone_number.length).join('')
     return a + b;
