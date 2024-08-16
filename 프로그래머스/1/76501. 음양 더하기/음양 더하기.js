993c420938e90d9b647be3207d7fcda53b3eb336
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-08-16 16:17:49 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,3 @@
 function solution(absolutes, signs) {
-    let result = [];
-    
-    for(i=0; i<signs.length; i++) {
-        result.push([absolutes[i], signs[i]])
-    }
-    
-    result.forEach(v => {
-        if(v[1] === false) {
-            v[0] *= -1
-        }
-    });
-    
-    return result.map(v => v[0]).reduce((a, b) => a + b);
+    return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
 }
