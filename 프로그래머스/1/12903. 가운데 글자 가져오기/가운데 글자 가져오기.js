651bdab920f5fa4530f68624f54e948216444162
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-08-16 16:38:04 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,5 @@
 function solution(s) {
     return s.length % 2 === 0 
-        ? [...s].slice(s.length/2-1, s.length/2+1).join('') 
+        ? s.slice(s.length/2-1, s.length/2+1)
         : s[~~(s.length/2)];
 }
