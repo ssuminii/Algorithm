e29c4b3b23d330623470f9d975f371a51ae6a89f
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-08-16 13:51:07 +0900

    [level 1] Title: 실패율, Time: 22.30 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,29 +1,24 @@
 function solution(N, stages) {
     let count = {};
     let result = [];
-
+    
+    
     stages.forEach(v => {
-        if (count[v] === undefined) {
+        if(count[v] === undefined) {
             count[v] = 0;
         }
         count[v] += 1;
-    }); 
-    // {"1":1,"2":3,"3":2,"4":1,"6":1}
+    })
     
-    
-    for (let i = 1; i <= N; i++) {
+    for(i=1; i<=N; i++) {
         let n = 0;
-        if (count[i] !== undefined) {
+        if(count[i] !== undefined) {
             n = count[i]
         }
-        let failRate = n / stages.length;
-        result.push([i, failRate]);
-        stages.length -= n;
+        let failRate = n/stages.length;
+        result.push([i,failRate])
+        stages.length -= n
     }
-    // [[1,0.125],[2,0.42857142857142855],[3,0.5],[4,0.5],[5,0]
-    // [[1,0],[2,0],[3,0],[4,1]]
+    return result.sort((a,b) => b[1] - a[1]).map(v => v[0]);
     
-    result.sort((a, b) => b[1] - a[1])
-    
-    return result.map(stage => stage[0]);
 }
